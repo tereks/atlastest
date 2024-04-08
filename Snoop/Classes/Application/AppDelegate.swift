d1a2436758bb88c7498700941d834a5c911220c0
Sergey Kim <sergey.kim.al@gmail.com>
2024-04-08 09:59:06 +0700

    feature 3

--- Snoop/Classes/Application/AppDelegate.swift
+++ Snoop/Classes/Application/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let rootRouter: RootRoutable = resolve()
         rootRouter.configure(window: window)
         
-        // feature 2
+        // feature 3
     }
 }
 
